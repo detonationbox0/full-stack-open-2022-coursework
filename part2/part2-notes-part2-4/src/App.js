17d7bd4e9b118434df2034c3af0e9ff30e267793
detonationbox0 <detonationbox0@live.com>
2023-02-23 13:01:56 -0500

    Finished 2C

--- part2/part2-notes-part2-4/src/App.js
+++ part2/part2-notes-part2-4/src/App.js
@@ -2,26 +2,23 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Note from './components/Note'
 
-
-
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
-  
+  const [showAll, setShowAll] = useState(false)
+
   const hook = () => {
-    
     console.log('effect')
     axios
       .get('http://localhost:3001/notes')
       .then(response => {
-        console.log("promise fulfilled")
+        console.log('promise fulfilled')
         setNotes(response.data)
-      });
-      
+      })
   }
 
   useEffect(hook, [])
-
+  
   console.log('render', notes.length, 'notes')
 
   const addNote = (event) => {
@@ -37,16 +34,24 @@ const App = () => {
   }
 
   const handleNoteChange = (event) => {
-    console.log(event.target.value)
     setNewNote(event.target.value)
   }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
 
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div> 
       <ul>
         <ul>
-          {notes.map(note => 
+          {notesToShow.map(note => 
             <Note key={note.id} note={note} />
           )}
         </ul>
