2d697440c09d36470a6c72af99f079f37e793b5e
detonationbox0 <detonationbox0@live.com>
2023-03-14 22:09:38 -0400

    Working on 2D

--- part2/part2-notes-part2-5/src/App.js
+++ part2/part2-notes-part2-5/src/App.js
@@ -3,41 +3,30 @@ import axios from 'axios'
 import Note from './components/Note'
 
 const App = () => {
-
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
-  const [showAll, setShowAll] = useState(false)
+  const [showAll, setShowAll] = useState(true)
 
-  const hook = () => {
-    console.log('effect')
+  useEffect(() => {
     axios
       .get('http://localhost:3001/notes')
       .then(response => {
-        console.log('promise fulfilled')
         setNotes(response.data)
       })
-  }
-
-  useEffect(hook, [])
-
-  console.log('render', notes.length, 'notes')
+  }, [])
 
   const addNote = (event) => {
     event.preventDefault()
-
     const noteObject = {
       content: newNote,
       important: Math.random() > 0.5,
     }
 
-    // Post the new note to json-server
     axios
       .post('http://localhost:3001/notes', noteObject)
       .then(response => {
-        console.log(response)
-        // Update the state
         setNotes(notes.concat(response.data))
-        setNewNote('') // then clear the field
+        setNewNote('')
       })
   }
 
@@ -45,34 +34,36 @@ const App = () => {
     setNewNote(event.target.value)
   }
 
-  const notesToShow = showAll
-    ? notes
-    : notes.filter(note => note.important)
-
-  const toggleImportanceOf = (id) => {
+  const toggleImportanceOf = id => {
 
     const url = `http://localhost:3001/notes/${id}`
     const note = notes.find(n => n.id === id)
-    const changedNote = { ...note, important: !note.important }
+    const changedNote = {...note, important: !note.important}
 
-    axios.put(url, changedNote)
-      .then(response => {
-        // Scratchin' my chin at below... === start here later
-        setNotes(notes.map(n => n.id !== id ? n : response.data))
-      })
+    axios.put(url, changedNote).then(response => {
+      // Sets the notes to what they currently are, minus the one we just changed
+      // If the map function catches this id, then use the response from
+      // the server because it's the latest version of this id data
+      setNotes(notes.map(n => n.id !== id ? n : response.data))
+    })
+
   }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
 
   return (
     <div>
       <h1>Notes</h1>
       <div>
         <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all'}
+          show {showAll ? 'important' : 'all' }
         </button>
-      </div>
+      </div> 
       <ul>
         <ul>
-          {notesToShow.map(note =>
+          {notesToShow.map(note => 
             <Note
               key={note.id}
               note={note}
